9abb53cc90ce7764f5f5dc71f0e3e79ccf9382bd
uchiborieri <zuumo219@gmail.com>
2023-06-12 15:01:42 +0900

    flashメッセージ

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,35 +1,55 @@
 class TaskController < ApplicationController
-  def new
-    @task = Task.new
-  end
 
   def index
     @task = Task.all
   end
 
-  def show
-    @task = Task.new(params[id])
-  end
-
-  def create
+  def new
     @task = Task.new
   end
 
+  def show
+  end
+
+  def create
+    if params[:back]
+      render :new
+    else
+      if @task.save
+        redirect_to tasks_path, notice: "タスクを作成しました!"
+      else
+        render :new
+      end
+    end
+  end
+
   def edit
-    @task = Task.find(params[id])
   end
 
   def update
-    @task = Task.find(params[id])
+    if @task.update(task_params)
+      redirect_to task_path, notice: "タスクを編集しました！"
+    else
+      render :edit
+    end
   end
 
   def destroy
-    @task = Task.find(params[id])
+    @task.destroy
+    redirect_to tasks_path, notice: "タスクを削除しました！"
+  end
+
+  def confirm
+    
   end
 
   private
 
+  def seto_task
+  @task = Task.find(params[id])
+  end
+
   def task_params
-    @task = task_params(params[id])
+    params.require(:task).permit(:title, :content)
   end
 end
