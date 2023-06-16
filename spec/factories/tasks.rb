e0c71f5c9e1faf00d016b2b460edfa98e50cb6d8
uchiborieri <zuumo219@gmail.com>
2023-06-16 15:38:31 +0900

    systemテスト

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,18 +2,24 @@ FactoryBot.define do
   factory :task do
     title { 'title' }
     content { 'task' }
+    limit_date { '2023-05-22' }
+    priority { '高' }
+    status { '完了' }
   end
 
-  FactoryBot.define do
-    factory :task1, class: Task do
-      title { 'title1' }
-      content { 'task1' }
+  factory :task1, class: Task do
+    title { 'title1' }
+    content { 'task1' }
+    limit_date { '2024-05-22' }
+    priority { '中' }
+    status { '完了' }
     end
 
-    factory :task2, class: Task do
-      title { 'title2' }
-      content { 'task2' }
-    end
+  factory :task2, class: Task do
+    title { 'title2' }
+    content { 'task2' }
+    limit_date { '2025-05-22' }
+    priority { '低' }
+    status { '完了' }
   end
-
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,15 +1,74 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
-  let!(:task)  {FactoryBot.create(:task, title: 'title')}
-  let!(:task1) { FactoryBot.create(:task1) }
-  let!(:task2) { FactoryBot.create(:task2) }
+  let!(:task)  {FactoryBot.create(:task, title: 'title', limit_date: '2023-05-22')}
+  let!(:task1) { FactoryBot.create(:task1, title: 'title1', limit_date:'2024-05-22') }
+  let!(:task2) { FactoryBot.create(:task2, title: 'title2', limit_date:'2025-05-22') }
   before do
     visit tasks_path
+  end
+  describe '優先順位での並び変え' do
+    context '優先順位をソートするボタンを押した場合' do
+      it '優先順位の昇順で表示される' do
+        click_on '優先順位をソートする'
+        sleep(1)
+        task_list = all('.priority_high')
+        expect(task_list[0]).to have_content '高'
+        expect(task_list[1]).to have_content '中'
+      end
+    end
+  end
+  describe '検索機能' do
+    context 'タイトルであいまい検索をした場合' do
+      it "検索キーワードを含むタスクで絞り込まれる" do
+        fill_in 'タイトル検索',with: 'title'
+        click_on '検索'
+        expect(page).to have_content 'title'
+      end
+    end
+    context 'ステータス検索をした場合' do
+      it "ステータスに完全一致するタスクが絞り込まれる" do
+        select '完了',from: 'ステータス検索'
+        expect(page).to have_select('ステータス検索', selected: '完了')
+      end
+    end
+    context 'タイトルのあいまい検索とステータス検索をした場合' do
+      it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
+        fill_in 'タイトル検索',with: 'title'
+        select '完了',from: 'ステータス検索'
+        click_on '検索'
+        expect(page).to have_content 'title'
+        expect(page).to have_select('ステータス検索', selected: '完了')
+      end
+    end
   end
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
+        visit new_task_path
+        fill_in 'task_title',with: 'title'
+        fill_in 'task_content',with: 'task'
+        select '2023',from: 'task_limit_date_1i'
+        select '5',from: 'task_limit_date_2i'
+        select '22',from: 'task_limit_date_3i'
+        select '着手中',from: 'task[status]'
+        select '高',from: 'task[priority]'
+        click_button '登録'
         expect(page).to have_content 'title'
+        expect(page).to have_content 'task'
+        expect(page).to have_content '2023-05-22'
+        expect(page).to have_content '着手中'
+        expect(page).to have_content '高'
+      end
+    end
+    context '終了期限でソートするボタンを押した場合' do
+      it '終了期限の降順で表示される' do
+        visit tasks_path
+        click_on '終了期限でソートする'
+        sleep(1)
+        task_list = all('.task_limit')
+        expect(task_list[0]).to have_content '2025-05-22'
+        expect(task_list[1]).to have_content '2024-05-22'
+        expect(task_list[2]).to have_content '2023-05-22'
       end
     end
   end
