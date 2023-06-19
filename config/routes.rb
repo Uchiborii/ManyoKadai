a10117668812fea1a460fb76547e51f16335acb2
uchiborieri <zuumo219@gmail.com>
2023-06-19 13:48:45 +0900

    ログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root to: 'tasks#index'
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
   resources :tasks do
     collection do
       post :confirm
