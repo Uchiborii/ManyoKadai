6a1a65947531728dabdaed299dbe51a8f67e3c9d
uchiborieri <zuumo219@gmail.com>
2023-06-19 13:15:07 +0900

    ログイン機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   validates :name, length: { maximum: 50 }, presence: true
   validates :email, length: { maximum: 50 }, uniqueness: true
-  validates :password_digest, length: { minimum: 6 }, presence: true
+  validates :password, length: { minimum: 6 }, presence: true
   before_validation { email.downcase! }
+  has_secure_password
   has_many :tasks
 end
