b4edb64303aa6fbadcf30e55607265429a4f64f8
uchiborieri <zuumo219@gmail.com>
2023-06-22 13:14:13 +0900

    ラベルのseed作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,14 @@
 User.create!(name:  "テストの人",
             email: "test@gmail.com",
-            password_digest:  "123456",
+            password:  "123456",
             )
+
+Label.destroy_all
+labels = [
+  { name: "日用品" },
+  { name: "娯楽" },
+  { name: "ファッション" },
+]
+labels.each do |label_params|
+  Label.create!(label_params)
+end
